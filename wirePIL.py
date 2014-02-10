9136a69fe62cc030705bfe153ebe2000d69de9cd
wcmckee <will@artcontrol.me>
2014-02-11 04:31:05 +1300

    saved imgs

--- wirePIL.py
+++ wirePIL.py
@@ -17,19 +17,97 @@ from PIL import ImageDraw
 import random
 import os
 from PIL import ImageChops
-import cocos
 
 # <codecell>
 
-os.chdir('/home/will/Desktop/output/')
+artimgs = ('/home/wcmckee/Pictures/art/wp-content/uploads/2013/02')
 
 # <codecell>
 
-chdirz = random.choice(os.listdir('/home/will/Desktop/output/'))
+os.chdir(artimgs)
 
 # <codecell>
 
-chdirz2 = random.choice(os.listdir('/home/will/Desktop/output/'))
+class Backup(object):
+    def findfiles(self, filestring):
+        filecur = os.listdir(os.curdir)
+        for filename in filecur:
+            if filestring in filename:
+                yield filename
+
+# <codecell>
+
+app = Backup()
+
+# <codecell>
+
+photolist = []
+
+# <codecell>
+
+for aviz in app.findfiles('1024x682.jpg'):
+    photolist.append(aviz)
+
+# <codecell>
+
+#print photolist
+
+# <codecell>
+
+photoamou = len(photolist)
+
+# <codecell>
+
+newliz2 = []
+
+# <codecell>
+
+for da in range(5):
+    ran = random.randint(0, photoamou)
+    print ran
+    zd = photolist[ran]
+    newliz2.append(zd)
+
+# <codecell>
+
+newliz = []
+
+# <codecell>
+
+for da in range(5):
+    ran = random.randint(0, photoamou)
+    print ran
+    zd = photolist[ran]
+    newliz.append(zd)
+
+# <codecell>
+
+print newliz[0:3]
+
+# <codecell>
+
+set(newliz).intersection(newliz2)
+
+# <codecell>
+
+ranznum = random.randint(0, photoamou)
+
+# <codecell>
+
+doubnum = ranznum + 1
+
+# <codecell>
+
+chdirz = photolist[ranznum]
+
+# <codecell>
+
+chdirz2 = photolist[doubnum]
+
+# <codecell>
+
+print chdirz2
+print chdirz
 
 # <codecell>
 
@@ -37,11 +115,14 @@ img2 = Image.open(chdirz)
 
 # <codecell>
 
-imgza2 = Image.open(chdirz)
+imgza2 = Image.open(chdirz2)
 
 # <codecell>
 
 imgAgain = img2.rotate(180)
+
+# <codecell>
+
 
 # <codecell>
 
@@ -57,10 +138,6 @@ enzSapz = ImageChops.subtract(imgSapz, img2)
 
 # <codecell>
 
-enzSapz.save('edit.jpg')
-
-# <codecell>
-
 imgLower = ImageChops.constant(imgAgain, 2)
 
 # <codecell>
@@ -69,19 +146,11 @@ imgOver = ImageEnhance.Brightness(imgLower)
 
 # <codecell>
 
-img4 = Image.open('edit.jpg')
-
-# <codecell>
-
-imgNever = ImageChops.blend(img4, imgAgain, .5)
-
-# <codecell>
-
 imgTitle = Image.open(chdirz)
 
 # <codecell>
 
-imgComt = ImageChops.blend(imgTitle, imgNever, .5)
+imgComt = ImageChops.blend(imgTitle, enzSapz, .5)
 
 # <codecell>
 
@@ -89,12 +158,12 @@ imgConvertz = ImageEnhance.Color(imgComt)
 
 # <codecell>
 
-img3 = ImageChops.screen(imgNever, img2)
+img3 = ImageChops.screen(imgComt, img2)
 
 # <codecell>
 
 
-bighImg = ImageChops.darker(img2, imgNever)
+bighImg = ImageChops.darker(img2, imgComt)
 
 # <codecell>
 
@@ -102,16 +171,7 @@ screen = ImageChops.difference(imgAgain,img2)
 
 # <codecell>
 
-screen.save('edit.jpg')
-
-# <codecell>
-
 brightLight = ImageEnhance.Brightness(screen)
-
-
-# <codecell>
-
-openFilz = Image.open('edit.jpg')
 
 # <codecell>
 
@@ -151,15 +211,15 @@ draw.rectangle([(0, 500), (1920, 1080)], fill='black')
 
 # <codecell>
 
-draw.rectangle([(90, 190), (370, 220)], fill=bakColr)
+#draw.rectangle([(90, 190), (370, 220)], fill=bakColr)
 
 # <codecell>
 
-draw.rectangle([(390, 290), (740, 320)], fill=bakColr)
+#draw.rectangle([(390, 290), (740, 320)], fill=bakColr)
 
 # <codecell>
 
-draw.rectangle([(590, 390), (1100, 420)], fill=bakColr)
+#draw.rectangle([(590, 390), (1100, 420)], fill=bakColr)
 
 # <codecell>
 
@@ -167,30 +227,18 @@ del draw
 
 # <codecell>
 
-img2.save("hello.PNG")
-
-# <codecell>
-
-helloz = Image.open("hello.PNG")
-
-# <codecell>
-
-os.chdir('/home/will/Desktop/newOut')
-
-# <codecell>
-
 cRan = random.randint(1,30)
 
 # <codecell>
 
-import ImageFont, ImageDraw
+#import ImageFont, ImageDraw
 
-draw = ImageDraw.Draw(lightFilz)
+#draw = ImageDraw.Draw(lightFilz)
 # use a truetype font
-font = ImageFont.truetype("cs.ttf", 24)
-draw.text((100, 200), "a film by William Mckee", fill=(49,cRan,2),font=font)
-draw.text((400, 300), 'Suburb Nightmare: Mist City', fill =(49,3,cRan),font=font)
-draw.text((600, 400), 'Based on the game: Suburb Warp Nighmare', fill=(cRan,3,2), font=font)
+#font = ImageFont.truetype("cs.ttf", 24)
+#draw.text((100, 200), "a film by William Mckee", fill=(49,cRan,2),font=font)
+#draw.text((400, 300), 'Suburb Nightmare: Mist City', fill =(49,3,cRan),font=font)
+#draw.text((600, 400), 'Based on the game: Suburb Warp Nighmare', fill=(cRan,3,2), font=font)
 
 
 # <codecell>
@@ -199,17 +247,28 @@ imgSwapz = ImageChops.difference(img2, fileSwap)
 
 # <codecell>
 
-lightFilz.save(chdirz)
+#lightFilz.show()
 
 # <codecell>
 
-lightFilz.show()
-
-# <codecell>
-
-imgBlack = ImageChops.darker(imgSwapz, imgNever)
+imgBlack = ImageChops.darker(imgSwapz, img2)
 
 # <codecell>
 
 imgZero = ImageChops.invert(imgBlack)
+
+# <codecell>
+
+#imgBlack.show()
+
+# <codecell>
+
+os.chdir('/home/wcmckee/Desktop/wireout')
+
+# <codecell>
+
+lightFilz.save(chdirz)
+
+# <codecell>
+
 
